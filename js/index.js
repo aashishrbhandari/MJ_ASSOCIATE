00292fe3e11a010a896ec59a2df4a0d3a6641a1c
Asharry97 <Asharry97@users.noreply.github.com>
2020-09-20 16:10:42 +0530

    major update, added new content, pdfs, table, whatapp updated, styling

--- js/index.js
+++ js/index.js
@@ -1,17 +1,38 @@
 // Get Navbar
 async function load_navbar() {
     document.querySelector("div.navbar-component").innerHTML = await (await fetch('/navbar.html')).text();
+    /*
+    console.log("Data-> ", document.querySelector(".sub-dropdown-menu"))
+    document.querySelector(".sub-dropdown-menu-btn").onclick = function () {
+        if (this.nextElementSibling.style.display === "block") {
+            this.nextElementSibling.style.display = "none"
+        } else {
+            this.nextElementSibling.style.display = "block";
+        }
+    }
+    */
+
 }
 
 // Get Footer
 async function load_footer() {
     document.querySelector("div.footer-component").innerHTML = await (await fetch('/footer.html')).text();
+
+    let disclaimer_modal = document.querySelector(".disclaimer-modal");
+    let disclaimer_modal_section = new bootstrap.Modal(disclaimer_modal, {
+        keyboard: false,
+        backdrop: 'static'
+    })
+
+    /*** A Very Basic Way [Testing] */
+    if (localStorage.getItem("Visited")) {
+        console.log("Visited No Need to Show Modal!!")
+    } else {
+        disclaimer_modal_section.show();
+        localStorage.setItem("Visited", "True")
+    }
+
 }
-
-// Handle Query Submit
-//document.querySelector("form.query-form").onsubmit = function () {
-
-//}
 
 function main() {
     // Main
@@ -20,3 +41,12 @@ function main() {
 }
 
 main();
+
+/* Testing
+
+window.onbeforeunload = function (event) {
+    localStorage.removeItem("Visited")
+};
+
+
+*/
